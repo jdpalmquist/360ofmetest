e712920f2861fa6529fc5bcf8a52b88c8f7f59cc
Jesse Palmquist <jesse.palmquist@gmail.com>
2016-10-10 17:03:47 -0700

    Still attempting to establish a stable, viable, full-access connection with the salesforce api... le sigh, this might kill my chances at getting this job. :(

---
+++ custom_modules/adapter_node_salesforce.js
@@ -0,0 +1,39 @@
+/* ADAPTER NODE-SALESFORCE: OAUTH AUTHORIZATION & QUERIES */
+
+
+//  LOAD THE CONFIG MODULES
+var cfg     = require('./config_salesforce');
+var sf 			= require('node-salesforce');
+
+
+var conn = new sf.Connection({
+    // you can change loginUrl to connect to sandbox or prerelease env. 
+    loginUrl : 'https://test.salesforce.com' 
+});
+
+conn.login(cfg.username, cfg.password, function(err, userInfo) {
+    if (err) { return console.error('ADAPATER_NODE_SALESFORCE --> connect() --> err: ',err); }
+    // Now you can get the access token and instance URL information. 
+    // Save them to establish connection next time. 
+    console.log(conn.accessToken);
+    console.log(conn.instanceUrl);
+    // logged in user property 
+    console.log("User ID: " + userInfo.id);
+    console.log("Org ID: " + userInfo.organizationId);
+    // ... 
+});
+
+
+function connect(){
+    if(conn == null){
+        
+    }else{
+
+    }
+}
+
+
+module.exports = {
+    connect: connect,
+
+};
--- custom_modules/rest_api_salesforce.js
+++ custom_modules/rest_api_salesforce.js
@@ -7,27 +7,8 @@ var cfg 		= require('./config_salesforce');
 
 // 	LOAD REQUIRED LIBRARIES
 var request 	= require('request');
-var sf 			= require('node-salesforce');
 
-
-var conn = new sf.Connection({
-  	// you can change loginUrl to connect to sandbox or prerelease env. 
-  	loginUrl : 'https://test.salesforce.com' 
-});
-conn.login(cfg.username, cfg.password, function(err, userInfo) {
-  if (err) { return console.error(err); }
-  // Now you can get the access token and instance URL information. 
-  // Save them to establish connection next time. 
-  console.log(conn.accessToken);
-  console.log(conn.instanceUrl);
-  // logged in user property 
-  console.log("User ID: " + userInfo.id);
-  console.log("Org ID: " + userInfo.organizationId);
-  // ... 
-});
-
-
-
+var adapter_sf 	= require('./adapter_node_salesforce');
 
 //
 function oauth_callback(req, res){
